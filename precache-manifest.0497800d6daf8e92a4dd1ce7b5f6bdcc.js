1e5a5ee022a984a8d4cfe9ed7ea0211640a2228c
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2020-06-29 02:59:22 +0530

    Updates

---
+++ precache-manifest.0497800d6daf8e92a4dd1ce7b5f6bdcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250f653be73a4c468a637ae8e6c9208a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b06070505d6c4d8ecafe",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "babd20c0d02cce64aef1",
+    "url": "./static/js/2.d37857c4.chunk.js"
+  },
+  {
+    "revision": "deba68208b35120ee58c1738e9406106",
+    "url": "./static/js/2.d37857c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06070505d6c4d8ecafe",
+    "url": "./static/js/main.8e9c6e6d.chunk.js"
+  },
+  {
+    "revision": "270924f67f65fb5d7bb7",
+    "url": "./static/js/runtime-main.eddad0fb.js"
+  }
+]);
